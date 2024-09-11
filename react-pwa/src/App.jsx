24e3cdd9a45324503288a28725c338ebe413c192
nuru <0xnuru@gmail.com>
2024-09-11 14:55:27 +0100

    feat: add router provider, router browser and create routes

--- react-pwa/src/App.jsx
+++ react-pwa/src/App.jsx
@@ -1,5 +1,26 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Login from "./pages/auth/Login";
+import LandingPage from "./pages/LandingPage";
+import MainLayout from "./layouts/MainLayout";
+import Register from "./pages/auth/Register";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<MainLayout />}>
+      <Route index element={<LandingPage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Route>
+  )
+);
+
 const App = () => {
-  return <h1>Hi</h1>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
