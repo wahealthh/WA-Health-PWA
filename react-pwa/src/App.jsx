f3548a5053441598815f37e31d1632ff7e4120c8
nuru <0xnuru@gmail.com>
2025-01-06 15:15:17 +0100

    chore: change chronic-care-management page to call history page

--- react-pwa/src/App.jsx
+++ react-pwa/src/App.jsx
@@ -4,11 +4,13 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
+import { Toaster } from "sonner";
 import Login from "./pages/auth/Login";
 import LandingPage from "./pages/LandingPage";
 import MainLayout from "./layouts/MainLayout";
 import Register from "./pages/auth/Register";
-import ChronicCareManagement from "./pages/ChronicCareManagement";
+import CallHistoryPage from "./pages/CallHistoryPage";
+import DuePatientsPage from "./pages/auth/DuePatientsPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -16,16 +18,19 @@ const router = createBrowserRouter(
       <Route index element={<LandingPage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route
-        path="/chronic-care-management"
-        element={<ChronicCareManagement />}
-      />
+      <Route path="/call-history" element={<CallHistoryPage />} />
+      <Route path="/due-patients" element={<DuePatientsPage />} />
     </Route>
   )
 );
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <Toaster />
+      <RouterProvider router={router} />
+    </>
+  );
 };
 
 export default App;
--- react-pwa/src/components/Footer.jsx
+++ react-pwa/src/components/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
   return (
     <footer className="flex h-14 w-full items-center justify-between bg-[#e7f1ff] px-6 shadow-lg text-[#004085]">
       <div className="flex items-center">
-        <span className="text-sm">© 2024 Chronic Care Management</span>
+        <span className="text-sm">© 2025 WA-Health</span>
       </div>
       <ul className="flex items-center gap-2">
         {footerItems.map(({ label, path }) => (
