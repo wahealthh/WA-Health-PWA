1b52c406e4d26d10f6e6d06f1ecc25587f87d9f4
0xnuru <0xnuru@gmail.com>
2025-01-22 15:48:39 +0100

    feat: move logic and API keys for fetching due patients to backend

--- react-pwa/src/pages/auth/DuePatientsPage.jsx
+++ react-pwa/src/pages/auth/DuePatientsPage.jsx
@@ -4,10 +4,7 @@ import Footer from "@/components/Footer";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import DuePatientsCard from "@/components/cards/cqm/DuePatientsCard";
 
-const API_URL =
-  "https://3b7d9ae6-db90-4d74-8073-723f7e08dd1c.mock.pstmn.io/recall_patients";
-const API_KEY =
-  "PMAK-673c96123988f500016fe83e-17f89587ed8187f30e052569918a49cbdb";
+const API_URL = "https://recall-product-backend.onrender.com/due_patients";
 
 const DuePatientsPage = () => {
   const [patients, setPatients] = useState([]);
@@ -17,12 +14,7 @@ const DuePatientsPage = () => {
   useEffect(() => {
     const fetchPatients = async () => {
       try {
-        const response = await fetch(`${API_URL}`, {
-          headers: {
-            "x-api-key": API_KEY,
-            "Content-Type": "application/json",
-          },
-        });
+        const response = await fetch(`${API_URL}`);
 
         if (!response.ok) throw new Error("Failed to fetch patients");
         const data = await response.json();
