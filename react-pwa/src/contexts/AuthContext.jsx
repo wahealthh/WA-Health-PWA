51eb67b2097186d4d0ea2c7a8116761a44d3b008
0xnuru <0xnuru@gmail.com>
2025-02-10 16:00:41 +0100

    feat: remove unused import

--- react-pwa/src/contexts/AuthContext.jsx
+++ react-pwa/src/contexts/AuthContext.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import { createContext, useContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect } from "react";
 import { toast } from "sonner";
 import Loading from "@/components/loading";
 
