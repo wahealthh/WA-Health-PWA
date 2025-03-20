8b6cc8e370777741276495c32f6b064222ed62dd
0xnuru <0xnuru@gmail.com>
2025-03-20 13:23:31 +0100

    chore: fix change login to token in uri

--- react-pwa/src/contexts/AuthContext.jsx
+++ react-pwa/src/contexts/AuthContext.jsx
@@ -37,7 +37,7 @@ export const AuthProvider = ({ children }) => {
         password: password,
       };
 
-      const response = await fetch("https://auth.wahealth.co.uk/auth/login", {
+      const response = await fetch("https://auth.wahealth.co.uk/auth/token", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
